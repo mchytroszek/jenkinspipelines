73ec010218eb3e2aa53c9b5b05eef0a190ee8b14
Michał Chytroszek <michal.chytroszek@gmail.com>
2017-10-18 17:31:16 +0200

    Added simple result archiving to test stage

--- jenkinspipelines/Jenkinsfile.groovy
+++ jenkinspipelines/Jenkinsfile.groovy
@@ -6,10 +6,15 @@ node('master') {
     }
 
     stage('Run tests') {
-        withMaven(maven: 'Maven 3') {
-            dir('jenkinspipelines') {
-                sh 'mvn clean test -Dwebdriver.type=remote -Dwebdriver.url=http://192.168.0.241:4444/wd/hub -Dwebdriver.cap.browserName=chrome'
+        try {
+            withMaven(maven: 'Maven 3') {
+                dir('jenkinspipelines') {
+                    sh 'mvn clean test -Dwebdriver.type=remote -Dwebdriver.url=http://192.168.0.241:4444/wd/hub -Dwebdriver.cap.browserName=chrome'
+                }
             }
+        } finally {
+            junit testResults: 'jenkinspipelines/target/*.xml', allowEmptyResults: true
+            archiveArtifacts 'jenkinspipelines/target/**'
         }
     }
 }
