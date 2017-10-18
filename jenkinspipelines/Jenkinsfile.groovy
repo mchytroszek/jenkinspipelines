b64efa40289430a82bfccf8ec3ff71973721548b
Michał Chytroszek <michal.chytroszek@gmail.com>
2017-10-18 17:26:27 +0200

    Changing webdriver path

--- jenkinspipelines/Jenkinsfile.groovy
+++ jenkinspipelines/Jenkinsfile.groovy
@@ -8,7 +8,7 @@ node('master') {
     stage('Run tests') {
         withMaven(maven: 'Maven 3') {
             dir('jenkinspipelines') {
-                sh 'mvn clean test -Dwebdriver.type=remote -Dwebdriver.url=http://localhost:4444/wd/hub -Dwebdriver.cap.browserName=chrome'
+                sh 'mvn clean test -Dwebdriver.type=remote -Dwebdriver.url=http://192.168.0.241:4444/wd/hub -Dwebdriver.cap.browserName=chrome'
             }
         }
     }
